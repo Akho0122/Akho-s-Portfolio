c49a93329cae370c4ebe524f90516cdac1fe9211
Akho0122 <mr.akhror.dev@gmail.com>
2022-01-11 03:47:59 +0500

    first commit

---
+++ src/Pages/ResumePage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Skills from "../Components/Skills";
+import {InnerLayout} from "../Styles/Layouts";
+import Title from "../Components/Title";
+function Resume(props) {
+    return (
+        <div>
+            <Skills/>
+            <Resume/>
+        </div>
+    );
+}
+
+export default Resume;
